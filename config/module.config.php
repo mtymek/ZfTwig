b9fb3ee5af82e028b63ead40ea2ddc835617753d
Mateusz Tymek <mtymek@gmail.com>
2012-03-17 16:59:03 +0100

    Di configuration

--- config/module.config.php
+++ config/module.config.php
@@ -3,25 +3,24 @@
 return array(
     'di' => array(
         'instance' => array(
-            /*'alias' => array(
-                'view'  => 'ZfTwig\Renderer',
-            ),*/
-            /*'ZfTwig\Renderer' => array(
+            'ZfTwig\TwigStrategy' => array(
+                'parameters' => array(
+                    'renderer' => 'ZfTwig\TwigRenderer',
+                ),
+            ),
+            'ZfTwig\TwigRenderer' => array(
                 'parameters' => array(
                     'environment' => 'ZfTwig\TwigEnvironment'
-                ),
-            ),*/
-            /*'ZfTwig\TwigEnvironment' => array(
+                )
+            ),
+            'ZfTwig\TwigEnvironment' => array(
                 'parameters' => array(
                     'loader' => 'ZfTwig\TemplateLoader',
-                    'broker' => 'Zend\View\HelperBroker',
                     'options' => array(
-                        'cache' => __DIR__ . '/../../../data/cache/twig',
-                        'auto_reload' => true,
-                        'debug' => true
+
                     )
                 )
-            )*/
+            )
         )
     )
 );
