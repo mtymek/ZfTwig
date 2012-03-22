4401ee6cb0c3c82b64cb769f78778f37ea7b4594
Mateusz Tymek <mtymek@gmail.com>
2012-03-22 23:53:17 +0100

    Use resolver for relative paths

--- src/ZfTwig/TemplateLoader.php
+++ src/ZfTwig/TemplateLoader.php
@@ -2,8 +2,48 @@
 
 namespace ZfTwig;
 
+use Zend\View\Resolver;
+
 class TemplateLoader implements \Twig_LoaderInterface
 {
+
+    /**
+     * Template resolver
+     *
+     * @var Resolver
+     */
+    protected $templateResolver;
+
+    /**
+     * Set script resolver
+     *
+     * @param \Zend\View\Resolver $resolver
+     * @return TemplateLoader
+     */
+    public function setResolver(Resolver $resolver)
+    {
+        $this->templateResolver = $resolver;
+        return $this;
+    }
+
+    /**
+     * Retrieve template name or template resolver
+     *
+     * @param  null|string $name
+     * @return string|Resolver
+     */
+    public function resolver($name = null)
+    {
+        if (null === $this->templateResolver) {
+            $this->setResolver(new Resolver\TemplatePathStack());
+        }
+
+        if (null !== $name) {
+            return $this->templateResolver->resolve($name, $this);
+        }
+
+        return $this->templateResolver;
+    }
 
     /**
      * Gets the source code of a template, given its name.
@@ -12,8 +52,11 @@ class TemplateLoader implements \Twig_LoaderInterface
      *
      * @return string The template source code
      */
-    function getSource($name)
+    public function getSource($name)
     {
+        if ($name[0] != '/') {
+            $name = $this->resolver($name);
+        }
         return file_get_contents($name);
     }
 
@@ -24,8 +67,11 @@ class TemplateLoader implements \Twig_LoaderInterface
      *
      * @return string The cache key
      */
-    function getCacheKey($name)
+    public function getCacheKey($name)
     {
+        if ($name[0] != '/') {
+            $name = $this->resolver($name);
+        }
         return $name;
     }
 
@@ -36,8 +82,11 @@ class TemplateLoader implements \Twig_LoaderInterface
      * @param timestamp $time The last modification time of the cached template
      * @return bool
      */
-    function isFresh($name, $time)
+    public function isFresh($name, $time)
     {
+        if ($name[0] != '/') {
+            $name = $this->resolver($name);
+        }
         return filemtime($name) < $time;
     }
 }
